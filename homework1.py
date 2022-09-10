d61b8dbd050956357b046b5de03634831ad60511
Yuliya Buday <yuliya.buday@gmal.com>
2022-09-10 22:01:09 +0300

    Add homework1

---
+++ homework1.py
@@ -0,0 +1,33 @@
+a = 17 / 2 * 3 + 2
+print('a=', a)
+b = 2 + 17 / 2 * 3
+print('b=', b)
+c = 19 % 4 + 15 / 2 * 3
+print('c=',c)
+d = (15 + 6) - 10 * 4
+print('d=',d)
+e = 17 / 2 % 2 * 3
+print('e=',e)
+
+
+a1 = 17 / 2 * (3 + 2)
+print('a1=', a1)
+b1 = (2 + 17) / 2 * 3
+print('b1=',b1)
+c1 = 19 % (4 + 15) / 2 * 3
+print('c1=',c1)
+d1 = 15 + (6 - 10) * 4
+print('d1=',d1)
+e1 = 17 / 2 % (2 * 3)
+print('e1=',e1)
+
+
+age_marina = 20
+age_oliya = 23
+age_ivana = 38
+print('summary age of all friends =',age_marina+age_oliya+age_ivana)
+print('average age =',(age_marina+age_oliya+age_ivana)/3)
+
+
+
+
