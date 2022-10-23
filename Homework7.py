d703b6c2911d5053fdb4a17c84d7d3a7f7f13edf
Yuliya Buday <yuliya.buday@gmal.com>
2022-10-23 12:28:25 +0300

    Add primary key autoinc

--- Homework7.py
+++ Homework7.py
@@ -5,17 +5,17 @@ with sq.connect('PhoneBook.db') as con:
 
     cur.execute("""
         CREATE TABLE IF NOT EXISTS Names(
-        ID INTEGER,
-        First Name TEXT,
+        ID INTEGER primary key AUTOINCREMENT,
+        "First Name" TEXT,
         Surname TEXT,
-        Phone Number VARCHAR
+        "Phone Number" TEXT
         )
 
         """
                 )
     con.commit()
-    users = [(1,"Simon","Howeis", "01223 349752"), (2,"Karen","Phillips", "01954 295773"),
-            (3,"Darren","Smith", "01583 749012"),(4,"Anne","Jones", "01323 567322"),
-            (5,"Mark","Smith", "01223 855534")]
-    cur.executemany("INSERT INTO Names VALUES(?, ?, ?, ?);", users)
+    users = [("Simon","Howeis", "01223 349752"), ("Karen","Phillips", "01954 295773"),
+            ("Darren","Smith", "01583 749012"),("Anne","Jones", "01323 567322"),
+            ("Mark","Smith", "01223 855534")]
+    cur.executemany(""" INSERT INTO Names ("First Name",Surname, "Phone Number") VALUES(?, ?, ?);""", users)
     con.commit()
